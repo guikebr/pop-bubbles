e73eac95ac1ff28f05b24c255069f40c329d702d
guilherme santos <guilhermesantos130997@gmail.com>
2021-08-09 19:30:04 -0300

    update relative import

--- lib/app/core/routes/app_pages.dart
+++ lib/app/core/routes/app_pages.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import '../../features/home/presentatiom/pages/home_binding.dart';
+import '../../features/home/presentatiom/pages/home_page.dart';
 import '../../features/unknown/presentation/pages/unknown_page.dart';
 
 part 'app_routes.dart';
@@ -15,5 +17,10 @@ abstract class AppPages {
 
   static final List<GetPage<dynamic>> routes = <GetPage<dynamic>>[
     unknown,
+    GetPage<HomePage>(
+      name: Routes.homePage,
+      page: () => const HomePage(),
+      binding: HomeBinding(),
+    ),
   ];
 }
--- lib/app/core/routes/app_routes.dart
+++ lib/app/core/routes/app_routes.dart
@@ -3,4 +3,5 @@ part of 'app_pages.dart';
 abstract class Routes {
   static const String rootPage = '/';
   static const String unknownPage = '/unknown_page';
+  static const String homePage = '/home_page';
 }
