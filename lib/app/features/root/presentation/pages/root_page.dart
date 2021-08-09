454a6a437abad21e1656b62cfba99d3b09ef0075
guilherme santos <guilhermesantos130997@gmail.com>
2021-08-09 19:29:51 -0300

    create root page and home page

---
+++ lib/app/features/root/presentation/pages/root_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'root_controller.dart';
+
+class RootPage extends GetView<RootController> {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
